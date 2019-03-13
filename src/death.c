576bf9cc96a8d7bcf84a49bf9731a002da8d4903
lucas <lucas.mascaut@epitech.eu>
2019-03-13 17:04:36 +0100

    adding file wich contain exploqion function

---
+++ src/death.c
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2019
+** death
+** File description:
+** defender
+*/
+
+#include "defender.h"
+
+void check_explose(monster_t *elem)
+{
+    if (elem->road == 8 && >= 1185) {
+	elem->alive = 0;
+    }
+}
