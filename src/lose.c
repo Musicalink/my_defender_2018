832ad9e0dbf0bfdc4fd81e4c795379be3da08f0f
lucas <lucas.mascaut@epitech.eu>
2019-03-17 19:55:20 +0100

    [ADD] adding fucntion wich display losing screen and the ressources

---
+++ src/lose.c
@@ -0,0 +1,31 @@
+/*
+** EPITECH PROJECT, 2019
+** lose
+** File description:
+** my_defender
+*/
+
+#include "defender.h"
+
+int display_lose(menu_t *lose, sfRenderWindow *window)
+{
+    sfEvent event;
+
+    while (sfRenderWindow_pollEvent(window, &event)) {
+        if (event.type == sfEvtClosed)
+            sfRenderWindow_close(window);
+    }
+    sfRenderWindow_clear(window, sfBlack);
+    sfRenderWindow_drawSprite(window, lose->sprite, NULL);
+}
+
+int lose(sfRenderWindow *window)
+{
+    menu_t *lose = init_menu("./ressources/lose.png");
+
+    while (sfRenderWindow_isOpen(window)) {
+        display_lose(lose, window);
+	sfRenderWindow_display(window);
+    }
+
+}
